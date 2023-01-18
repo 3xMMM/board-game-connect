0f134ddd5094499ed383e6f1e4839eaa169be724
Mark Martinez <20521175+3xMMM@users.noreply.github.com>
2023-01-18 14:07:16 -0800

    WIP on getting sessions working.

--- backend/app.ts
+++ backend/app.ts
@@ -1,13 +1,17 @@
 import express, { Request, Response } from 'express';
 // import * as dotenv from 'dotenv';
-import { Client } from 'pg';
+import { Pool } from 'pg';
 import session from 'express-session';
+import createSessionStore from 'connect-pg-simple';
+
+const SessionStore = createSessionStore(session);
 
 // dotenv.config();
 
 const app = express();
 const port = process.env.PORT ?? 4000;
-const client = new Client({
+
+const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
@@ -15,19 +19,34 @@ const client = new Client({
   port: parseInt(process.env.DB_PORT ?? '5432')
 });
 
-// app.use(
-//   session({
-//
-//   })
-// );
+app.use(
+  session({
+    store: new SessionStore({
+      pool
+    }),
+    secret: 'hey',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
+    // Insert express-session options here
+  })
+);
 
-void client.connect().then(() => {
-  // eslint-disable-next-line n/handle-callback-err
-  client.query('SELECT NOW()', (err, res) => {
-    console.log(res.rows);
-    void client.end();
-  });
+pool.query('SELECT NOW()', (err, res) => {
+  if (typeof err !== 'undefined') {
+    console.error(err);
+    return;
+  }
+  console.log(res.rows);
 });
+
+// void client.connect().then(() => {
+//   // eslint-disable-next-line n/handle-callback-err
+//   client.query('SELECT NOW()', (err, res) => {
+//     console.log(res.rows);
+//     void client.end();
+//   });
+// });
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Home Route');
