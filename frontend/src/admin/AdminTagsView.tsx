aa784b4ffa028b0ca8926330aac2bac568e5cc38
Mark Martinez <20521175+3xMMM@users.noreply.github.com>
2023-02-14 09:07:52 -0800

    Added styling to View page.

--- frontend/src/admin/AdminTagsView.tsx
+++ frontend/src/admin/AdminTagsView.tsx
@@ -2,10 +2,11 @@ import {
     Table,
     Thead,
     Tbody,
+    Text,
     Tr,
     Th,
     Td,
-    TableContainer, Heading, Container
+    TableContainer, Heading, Container, Button, Flex, Divider
 } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { Tag } from '../spa';
@@ -24,12 +25,20 @@ export default function AdminTagsView() {
     return (
         <div>
             <Container id='AdminTagsView' maxW='container.xl'>
-                <Heading as='h1'>Tags</Heading>
+                <Heading as='h1' my='4'>Tags</Heading>
+                <Text mb='5'><strong>Tags</strong> are used to assign categories and taxonomies to <strong>Games</strong>. A <strong>Game</strong> can have many <strong>Tags</strong>.</Text>
+                <Divider/>
+                <Flex justifyContent='space-between' alignItems='center'>
+                    <Text display='inline-flex'><strong><em>{tags.length} tag(s)</em></strong></Text>
+                    <Button my='4'>Add Tag</Button>
+                </Flex>
                 <TableContainer>
-                    <Table variant='striped' colorScheme='primary'>
+                    <Table variant='striped' colorScheme='primary' size='sm'>
                         <Thead>
                             <Tr>
                                 <Th>Tag Name</Th>
+                                <Th>Games w/ Tag</Th>
+                                <Th>Action</Th>
                             </Tr>
                         </Thead>
                         <Tbody>
@@ -38,6 +47,8 @@ export default function AdminTagsView() {
                                     return (
                                         <Tr key={tag.name}>
                                             <Td>{ tag.name }</Td>
+                                            <Td>TBD</Td>
+                                            <Td>TBD</Td>
                                         </Tr>
                                     );
                                 })
